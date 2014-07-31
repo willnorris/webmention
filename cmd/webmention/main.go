773fc1942f519c3af1a9fe1895c50b83f216b99f
Will Norris <will@willnorris.com>
2014-07-31 08:43:10 -0700

    add license and package comment to cmd/webmention

--- cmd/webmention/main.go
+++ cmd/webmention/main.go
@@ -1,3 +1,11 @@
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// The webmention binary is a command line utiltiy for sending webmentions to
+// the URLs linked to by a given webpage.
 package main
 
 import (
